37da6ecbf70103946646b50ea79b52f232c360bb
Hellhackee <bobylevalexandr93@gmail.com>
2021-02-18 02:08:21 +0400

    Добавил бэкграунд, партиклы

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,19 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
-[RequireComponent(typeof(Rigidbody2D))]
+
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(CircleCollider2D))]
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private int _damage;
     [SerializeField]private float _speed;
+    [SerializeField] private float _dieScaleMin;
+    [SerializeField] private float _dieScaleMax;
+    [SerializeField] private float _dieAlpha;
 
     private Vector3 _direction;
     private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer _spriteRenderer;
+    private CircleCollider2D _circleCollider2D;
+    private Color _dieColor = new Color(Color.gray.r, Color.gray.g, Color.gray.b, 0.2f);
+
+    private void OnValidate()
+    {
+        if (_dieScaleMax < _dieScaleMin)
+        {
+            _dieScaleMax = _dieScaleMin;
+        }
+    }
 
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _circleCollider2D = GetComponent<CircleCollider2D>();
     }
 
     private void Update()
@@ -25,8 +43,7 @@ public class Enemy : MonoBehaviour
     {
         if (collision.GetComponent<Ink>())
         {
-            Destroy(collision.gameObject);
-            Destroy(gameObject);
+            DieByInk();
         }
 
         if (collision.TryGetComponent<Player>(out Player player))
@@ -39,5 +56,15 @@ public class Enemy : MonoBehaviour
     public void SetDirection(Vector3 direction)
     {
         _direction = (direction - transform.position).normalized;
+    }
+
+    private void DieByInk()
+    {
+        float dieScale = Random.Range(_dieScaleMin, _dieScaleMax);
+
+        _spriteRenderer.DOColor(_dieColor, 1f);
+        transform.DOScale(dieScale, 1f);
+        _direction = Vector2.up;
+        _circleCollider2D.enabled = false;
     }
 }
