ab65587b4ba43a7cb5936e4428978a5e912cd771
Hellhackee <bobylevalexandr93@gmail.com>
2021-02-22 00:38:18 +0400

    added background and new sprites

---
+++ Assets/Scripts/Background/LightIntensityAnim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(Light))]
+public class LightIntensityAnim : MonoBehaviour
+{
+    [SerializeField] private float _minIntensity;
+    [SerializeField] private float _maxIntensity;
+    [SerializeField] private float _loopTime;
+
+    private Light _light;
+
+    private void Start()
+    {
+        _light = GetComponent<Light>();
+        _light.intensity = _minIntensity;
+        _light.DOIntensity(_maxIntensity, _loopTime).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
+    }
+}
---
+++ Assets/Scripts/Octopus/Gun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Gun : MonoBehaviour
+{
+    private Animator _animator;
+    [SerializeField] private AttackPoint _point;
+    [SerializeField] private float _timeForGunBusy;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        _point.IsReloading += ReloadGun;
+    }
+
+    private void OnDisable()
+    {
+        _point.IsReloading -= ReloadGun;
+    }
+
+    private void ReloadGun()
+    {
+        StartCoroutine(Reload());
+    }
+
+    private IEnumerator Reload()
+    {
+        _point.IsBusy = true;
+        _animator.Play("ReloadGun");
+        yield return new WaitForSeconds(_timeForGunBusy);
+
+        _point.IsBusy = false;
+    }
+}
